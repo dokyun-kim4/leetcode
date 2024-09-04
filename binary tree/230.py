ba862bcf4686eb89641705ecce89ffd7c13b3f2d
Dokyun Kim <donny5557@gmail.com>
2024-09-04 15:33:58 -0400

    230

--- binary tree/230.py
+++ binary tree/230.py
@@ -4,22 +4,28 @@ from binary_tree import *
 
 
 def answer(root: TreeNode, k: int):
-    path = []
+    res = None
 
     def dfs(node):
+        nonlocal res
+        nonlocal k
         if not node:
             return
         else:
+
             dfs(node.left)
-            path.append(node.val)
-            dfs(node.right)
+            k -= 1
+            if k == 0:
+                res = node.val
+            if k > 0:
+                dfs(node.right)
 
     dfs(root)
-    return path[k - 1]
+    return res
 
 
 if __name__ == "__main__":
     root = [3, 1, 4, None, 2]
-    k = 1
+    k = 2
     root = to_binary_tree(root)
     print(answer(root, k))
