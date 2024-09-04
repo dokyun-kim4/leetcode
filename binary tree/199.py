f35d179bb3e6bbdefa47344e14e8ed1988716690
Dokyun Kim <donny5557@gmail.com>
2024-09-04 15:39:59 -0400

    199

---
+++ binary tree/199.py
@@ -0,0 +1,31 @@
+# Binary Tree Right Side View
+
+
+from binary_tree import *
+
+
+def answer(root: TreeNode):
+
+    if root is None:
+        return []
+
+    visited = []
+    queue = [root]
+
+    while len(queue) > 0:
+        levelLength = len(queue)
+        curLevel = []
+        for _ in range(levelLength):
+            curNode = queue.pop(0)
+            curLevel.append(curNode.val)
+            if curNode.left is not None:
+                queue.append(curNode.left)
+            if curNode.right is not None:
+                queue.append(curNode.right)
+        visited.append(curLevel[-1])
+    return visited
+
+
+if __name__ == "__main__":
+    root = to_binary_tree([3, 9, None, None, None, 15, 7])
+    print(answer(root))
